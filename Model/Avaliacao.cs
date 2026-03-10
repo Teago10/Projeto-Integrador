ce308944c19e1fedd7b39aaba369a86f353988a3
thiago Affonso <thiagoaffonso452@gmail.com>
2026-03-09 22:14:00 -0300

    Atualizações do sistema

---
+++ Model/Avaliacao.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestaoCuidadores.Model
+{
+    public class Avaliacao
+    {
+        public int Id { get; set; }
+        public int IdCuidador { get; set; }
+        public int IdCliente { get; set; }
+        public int Nota { get; set; }
+        public string Comentario { get; set; }
+    }
+}
