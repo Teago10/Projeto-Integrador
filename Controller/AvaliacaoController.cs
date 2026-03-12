ddf5024079f96502d9281b049316495e23b7bdea
thiago Affonso <thiagoaffonso452@gmail.com>
2026-03-11 21:31:16 -0300

    Implementação do código para pegar os ids do responsavel e do cuidador

---
+++ Controller/AvaliacaoController.cs
@@ -0,0 +1,36 @@
+﻿using GestaoCuidadores.Model;
+using GestaoCuidadores.Repository;
+using GestaoCuidadores.View;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestaoCuidadores.Controller
+{
+    public class AvaliacaoController
+    {
+        private FrmAvalicao _frmAvalicao;
+        private AvaliacaoRepository _avaliacaoRepository;
+
+        public AvaliacaoController(FrmAvalicao view)
+        {
+            _frmAvalicao = view;
+            _avaliacaoRepository = new AvaliacaoRepository();
+        }
+
+        public void SalvarAvaliacao(Avaliacao avaliacao)
+        {
+            try
+            {
+                _avaliacaoRepository.Inserir(avaliacao);
+                //_frmAvalicao.ExibirMensagem("Avaliação salva com sucesso!");
+            }
+            catch (Exception ex)
+            {
+                //_frmAvalicao.ExibirMensagem($"Erro ao salvar avaliação: {ex.Message}" );
+            }
+        }
+    }
+}
--- View/FrmAvalicao.cs
+++ View/FrmAvalicao.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using GestaoCuidadores.Controller;
+using GestaoCuidadores.Model;
+using GestaoCuidadores.Repository;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +15,11 @@ namespace GestaoCuidadores.View
 {
     public partial class FrmAvalicao : Form
     {
+        private AvaliacaoController _avaliacaoController;
         public FrmAvalicao()
         {
             InitializeComponent();
+            _avaliacaoController = new AvaliacaoController(this);
         }
 
         private void FrmAvalicao_Load(object sender, EventArgs e)
@@ -24,7 +29,30 @@ namespace GestaoCuidadores.View
 
         private void btnSalvar_Click(object sender, EventArgs e)
         {
+            try
+            {
+                Avaliacao avaliacao = new Avaliacao();
 
+                //pega o Id do cuidador e do responsável selecionados nos combo boxes
+                avaliacao.Id_Cuidador = Convert.ToInt32(cbxNomeCuidador.SelectedValue);
+                avaliacao.Id_Responsavel = Convert.ToInt32(cbxNomeResponsavel.SelectedValue);
+
+                avaliacao.Nota = Convert.ToInt32(cbxNota.Text);
+                avaliacao.Comentario = txtComentario.Text;
+
+                _avaliacaoController.SalvarAvaliacao(avaliacao);
+
+                MessageBox.Show("Avaliação salva com sucesso!", "Sucesso", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ocorreu um erro ao salvar a avaliação:{ex.Message} ", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        public void ExibirMensagem(string mensagem)
+        {
+            MessageBox.Show(mensagem);
         }
     }
 }
